0fb6a970ce7450439866ebb5bff52eafaaa6f717
K.T.Mirror <z1022001@gmail.com>
2019-05-24 00:34:39 +0800

    0.7.1.0
        retweet Image
        tweet object add media
        auto retweet image
        
        update log fix
        log target fix

--- config.js
+++ config.js
@@ -5,15 +5,17 @@
 
 // commit
 /*
+    0.7.1.0
     retweet Image
     tweet object add media
     auto retweet image
+    
     update log fix
     log target fix
 */
 
 const config = {
-    _version: "0.7.0.3",
+    _version: "0.7.1.0",
     // 主版本號：當你做了不兼容的API修改
     // 次版本號：當你做了向下兼容的功能性新增
     // 修訂號：當你做了向下兼容的問題修正
