f90d5d10fd82619dddf34be239412af0f15c67a9
K.T.(HUA-VPC) <z1022001jp@gmail.com>
2020-07-24 16:49:59 +0800

    gitflow-hotfix-stash: 0.8.7/imgurApiUpgrade

--- anna.js
+++ anna.js
@@ -412,12 +412,12 @@ const replyAI = _anna.replyAI = async function (rawMsg, sourceId, userId) {
                         return "分類錯誤!";
                     }
 
-                    // set taglist
+                    // set new taglist
                     await imgur.api.image.updateImage({ imageHash: imgArray[0].id, tagList: "/Images/Character/" + target + "/" + imgArray[0].fileName });
 
-                    // update imgur database
-                    imgur.database.deleteImageData({ id: imgArray[0].id });
-                    imgur.api.image.image({ imageHash: imgArray[0].id });
+                    // // update imgur database
+                    // imgur.database.deleteImageData({ id: imgArray[0].id });
+                    // imgur.api.image.image({ imageHash: imgArray[0].id });
 
                     // return "分類完成";
                     return line.createMsgButtons("分類完成", [">>" + target, "next"], ["https://aigis1000secretary.herokuapp.com/images/" + target, "new"]);
