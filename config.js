ac26831f7b0667c6802aecc6d78a9eb522956c47
K.T.(HUA-VPC) <z1022001@gmail.com>
2020-09-25 16:09:43 +0800

    0.9.0.0

--- config.js
+++ config.js
@@ -26,7 +26,7 @@
 
 const crypto = require("./crypto.js");
 const _config = module.exports = {
-    _version: "0.8.8.11",
+    _version: "0.9.0.0",
     // 主版本號：當你做了不兼容的API修改
     // 次版本號：當你做了向下兼容的功能性新增
     // 修訂號：當你做了向下兼容的問題修正
