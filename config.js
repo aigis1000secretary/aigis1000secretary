c9e4d2b7ccbbc90cce02f0f5eab55da41380ccd3
K.T.(HUA-VPC) <z1022001jp@gmail.com>
2021-03-30 13:51:36 +0800

    gitflow-hotfix-stash: 1.1.0/twitterPushLimitFix

--- config.js
+++ config.js
@@ -48,12 +48,15 @@
 
     1.1.0.8
     new img move path fix
+
+    1.1.0.9
+    line twitter push limit
 */
 
 const crypto = require("./crypto.js");
 const dbox = require("./dbox.js");
 let _config = module.exports = {
-    _version: "1.1.0.8",
+    _version: "1.1.0.9",
     // 主版本號：當你做了不兼容的API修改
     // 次版本號：當你做了向下兼容的功能性新增
     // 修訂號：當你做了向下兼容的問題修正
