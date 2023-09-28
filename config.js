9c529df93011d310ce0ea082d768ed37bf4b646a
K.T <z1022001@gmail.com>
2023-09-28 13:53:36 +0800

    gitflow-hotfix-stash: 1.3.0/retweetGetRealUrl

--- config.js
+++ config.js
@@ -18,12 +18,16 @@
 
 /*
     // commit 
+
+    1.3.0.1
+    retweet get real url
+
 */
 
 const crypto = require("./crypto.js");
 const dbox = require("./dbox.js");
 let _config = module.exports = {
-    _version: "1.3.0.0",
+    _version: "1.3.0.1",
     // 主版本號：當你做了不兼容的API修改
     // 次版本號：當你做了向下兼容的功能性新增
     // 修訂號：當你做了向下兼容的問題修正
