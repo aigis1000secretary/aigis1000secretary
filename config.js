b99d3b2ae3858b5f37bb4b1b0eddae23b1da07e4
K.T <z1022001@gmail.com>
2024-03-02 16:08:56 +0800

    gitflow-hotfix-stash: 1.3.0/dockerfile

--- config.js
+++ config.js
@@ -48,12 +48,15 @@
     1.3.0.9
     imgur fix images upload script
 
+    1.3.0.10
+    set dockerfile for fly.io
+
 */
 
 const crypto = require("./crypto.js");
 const dbox = require("./dbox.js");
 let _config = module.exports = {
-    _version: "1.3.0.9",
+    _version: "1.3.0.10",
     // 主版本號：當你做了不兼容的API修改
     // 次版本號：當你做了向下兼容的功能性新增
     // 修訂號：當你做了向下兼容的問題修正
